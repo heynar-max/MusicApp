41afd48c0f913a76139dcff29f1f8cd6ec0ae6cd
heynar-max <heynar76@hotmail.com>
2024-04-27 16:33:51 -0500

    Data

---
+++ src/lib/data.js
@@ -0,0 +1,242 @@
+export const playlists = [
+    {
+        id: '1',
+        albumId: 1,
+        title: "Mas Alla De La Realidad",
+        cover:
+            "https://res.cloudinary.com/dzty81hol/image/upload/v1714233374/xqlham3gwdkzkfdais10.jpg",
+        artists: ["AKASH"],
+    },
+    {
+        id: '2',
+        albumId: 2,
+        title: "AM",
+        cover:
+            "https://res.cloudinary.com/dzty81hol/image/upload/v1714233474/bbazezygjc6isnmsaikk.jpg",
+        artists: ["ARTIC MOCKEY"],
+    },
+    {
+        id: '3',
+        albumId: 3,
+        title: "Resurreción",
+        cover:
+            "https://res.cloudinary.com/dzty81hol/image/upload/v1714233571/rx2vnlmvu7frngc4tqjx.jpg",
+        artists: ["AKASH"],
+    },
+    {
+        id: '4',
+        albumId: 4,
+        title: "Kronos",
+        cover:
+            "https://res.cloudinary.com/dzty81hol/image/upload/v1714233673/j6x4oor8lhfd5j8yeoa4.jpg",
+        artists: ["KRONOS"],
+    },
+    {
+        id: '5',
+        albumId: 5,
+        title: "Le Mua",
+        cover:
+            "https://res.cloudinary.com/dzty81hol/image/upload/v1714233768/dttyn0ybxhkfcuqviie5.jpg",
+        artists: ["LE MUA"],
+    },
+    {
+        id: '5',
+        albumId: 5,
+        title: "REMIX",
+        cover:
+            "https://res.cloudinary.com/dzty81hol/image/upload/v1714233861/o2eqjyznckselkg8br6e.jpg",
+        artists: ["DUKE DOMONT","ROGER SANCHEZ","TENNEBRECK","QODES","BOB SINCLAR"],
+    },
+]
+
+export const song = [
+    {
+        "id": 1,
+        "albumId": 1,
+        "title": "Recuerdos",
+        "image": `https://res.cloudinary.com/dzty81hol/image/upload/v1714233374/xqlham3gwdkzkfdais10.jpg`,
+        "artists": ["Akash"],
+        "album": "Mas Alla De La Realidad",
+        "duration": "3:53"
+    },
+    {
+        "id":2 ,
+        "albumId": 1,
+        "title": "Sol y Luna",
+        "image": `https://res.cloudinary.com/dzty81hol/image/upload/v1714233374/xqlham3gwdkzkfdais10.jpg`,
+        "artists": ["Akash"],
+        "album": "Mas Alla De La Realidad",
+        "duration": "3:51"
+    },
+    {
+        "id": 3,
+        "albumId": 1,
+        "title": "Una Sombra a la Atardecer",
+        "image": `https://res.cloudinary.com/dzty81hol/image/upload/v1714233374/xqlham3gwdkzkfdais10.jpg`,
+        "artists": ["Akash"],
+        "album": "Mas Alla De La Realidad",
+        "duration": "6:46"
+    },
+    {
+        "id": 1,
+        "albumId": 2,
+        "title": "I Wanna Be Yours",
+        "image": `https://res.cloudinary.com/dzty81hol/image/upload/v1714233474/bbazezygjc6isnmsaikk.jpg`,
+        "artists": ["Arctic Monkeys"],
+        "album": "AM",
+        "duration": "3:04"
+    },
+    {
+        "id": 2,
+        "albumId": 2,
+        "title": "Why'd You Only Call Me When You're High?",
+        "image": `https://res.cloudinary.com/dzty81hol/image/upload/v1714233474/bbazezygjc6isnmsaikk.jpg`,
+        "artists": ["Arctic Monkeys"],
+        "album": "AM",
+        "duration": "2:41"
+    },
+    {
+        "id": 3,
+        "albumId": 2,
+        "title": "Do I Wanna Know?",
+        "image": `https://res.cloudinary.com/dzty81hol/image/upload/v1714233474/bbazezygjc6isnmsaikk.jpg`,
+        "artists": ["Arctic Monkeys"],
+        "album": "AM",
+        "duration": "2:21"
+    },
+    {
+        "id": 4,
+        "albumId": 2,
+        "title": "Snap Out Of It",
+        "image": `https://res.cloudinary.com/dzty81hol/image/upload/v1714233474/bbazezygjc6isnmsaikk.jpg`,
+        "artists": ["Arctic Monkeys"],
+        "album": "AM",
+        "duration": "3:16"
+    },
+    {
+        "id": 1,
+        "albumId": 3,
+        "title": "Luchar y Perder",
+        "image": `https://res.cloudinary.com/dzty81hol/image/upload/v1714233571/rx2vnlmvu7frngc4tqjx.jpg`,
+        "artists": ["Akash"],
+        "album": "Resurreción",
+        "duration": "3:35"
+    },
+    {
+        "id": 2,
+        "albumId": 3,
+        "title": "Tu Sombra En Mi Pared",
+        "image": `https://res.cloudinary.com/dzty81hol/image/upload/v1714233571/rx2vnlmvu7frngc4tqjx.jpg`,
+        "artists": ["Akash"],
+        "album": "Resurreción",
+        "duration": "4:57"
+    },
+    {
+        "id": 1,
+        "albumId": 4,
+        "title": "Fuego En Mis Venas",
+        "image": `https://res.cloudinary.com/dzty81hol/image/upload/v1714233673/j6x4oor8lhfd5j8yeoa4.jpg`,
+        "artists": ["Kronos"],
+        "album": "Kronos Exitos",
+        "duration": "5:20"
+    },
+    {
+        "id": 2,
+        "albumId": 4,
+        "title": "Igual Que Ayer",
+        "image": `https://res.cloudinary.com/dzty81hol/image/upload/v1714233673/j6x4oor8lhfd5j8yeoa4.jpg`,
+        "artists": ["Kronos"],
+        "album": "Kronos Exitos",
+        "duration": "4:06"
+    },
+    {
+        "id": 3,
+        "albumId": 4,
+        "title": "Todo Esta Bien",
+        "image": `https://res.cloudinary.com/dzty81hol/image/upload/v1714233673/j6x4oor8lhfd5j8yeoa4.jpg`,
+        "artists": ["Kronos"],
+        "album": "Kronos Exitos",
+        "duration": "3:48"
+    },
+    {
+        "id": 4,
+        "albumId": 4,
+        "title": "Volver a Empezar",
+        "image": `https://res.cloudinary.com/dzty81hol/image/upload/v1714233673/j6x4oor8lhfd5j8yeoa4.jpg`,
+        "artists": ["Kronos"],
+        "album": "Kronos Exitos",
+        "duration": "6:56"
+    },
+    {
+        "id": 1,
+        "albumId": 5,
+        "title": "Abre Los Ojos",
+        "image": `https://res.cloudinary.com/dzty81hol/image/upload/v1714233768/dttyn0ybxhkfcuqviie5.jpg`,
+        "artists": ["Le Mua"],
+        "album": "Le Mua Exitos",
+        "duration": "3:30"
+    },
+    {
+        "id": 2,
+        "albumId": 5,
+        "title": "Otros Labios",
+        "image": `https://res.cloudinary.com/dzty81hol/image/upload/v1714233768/dttyn0ybxhkfcuqviie5.jpg`,
+        "artists": ["Le Mua"],
+        "album": "Le Mua Exitos",
+        "duration": "3:38"
+    },
+    {
+        "id": 3,
+        "albumId": 5,
+        "title": "Senti-Grados",
+        "image": `https://res.cloudinary.com/dzty81hol/image/upload/v1714233768/dttyn0ybxhkfcuqviie5.jpg`,
+        "artists": ["Le Mua"],
+        "album": "Le Mua Exitos",
+        "duration": "4:45"
+    },
+    {
+        "id": 1,
+        "albumId": 6,
+        "title": "Ocean Drive",
+        "image": `https://res.cloudinary.com/dzty81hol/image/upload/v1714233861/o2eqjyznckselkg8br6e.jpg`,
+        "artists": ["Duke Dumont"],
+        "album": "Duality",
+        "duration": "3:27"
+    },
+    {
+        "id": 2,
+        "albumId": 6,
+        "title": "Lost",
+        "image": `https://res.cloudinary.com/dzty81hol/image/upload/v1714233861/o2eqjyznckselkg8br6e.jpg`,
+        "artists": ["Roger Sanchez FT Luisa Pure"],
+        "album": "Come With Me",
+        "duration": "4:49"
+    },
+    {
+        "id": 3,
+        "albumId": 6,
+        "title": "Nina",
+        "image": `https://res.cloudinary.com/dzty81hol/image/upload/v1714233861/o2eqjyznckselkg8br6e.jpg`,
+        "artists": ["Tennebreck"],
+        "album": "Nina Sencillo",
+        "duration": "4:20"
+    },
+    {
+        "id": 4,
+        "albumId": 6,
+        "title": "Poison",
+        "image": `https://res.cloudinary.com/dzty81hol/image/upload/v1714233861/o2eqjyznckselkg8br6e.jpg`,
+        "artists": ["Qodes FT Athena"],
+        "album": "Poison Sencillo",
+        "duration": "4:20"
+    },
+    {
+        "id": 5,
+        "albumId": 6,
+        "title": "Cinderella",
+        "image": `https://res.cloudinary.com/dzty81hol/image/upload/v1714233861/o2eqjyznckselkg8br6e.jpg`,
+        "artists": ["Bob Sinclar"],
+        "album": "Paris By Night",
+        "duration": "3:02"
+    },
+]
